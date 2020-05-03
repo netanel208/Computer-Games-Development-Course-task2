73ea3ec64c711f24bb06dc73522172d3ea519a29
netanel <orez132@gmail.com>
2020-05-03 21:43:01 +0300

    Update file

---
+++ collisions/Assets/Scripts/3-collisions/WallThePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallThePlayer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject.FindGameObjectWithTag("Player").GetComponent<KeyboardMover>().enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<KeyboardMover>().enabled = false;
+            if(this.gameObject.tag == "DownWall")
+            {
+                other.GetComponent<Transform>().position += new Vector3(0,0.3f,0);
+            } else if(this.gameObject.tag == "UpWall") {
+                other.GetComponent<Transform>().position += new Vector3(0,-0.3f, 0);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<KeyboardMover>().enabled = true;
+    }
+
+
+}
