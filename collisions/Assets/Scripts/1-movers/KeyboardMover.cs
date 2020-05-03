016dbf5f0e61e144d7497cff4536c8d73bbd57ee
netanel <orez132@gmail.com>
2020-05-03 22:25:44 +0300

    Update files

--- collisions/Assets/Scripts/1-movers/KeyboardMover.cs
+++ collisions/Assets/Scripts/1-movers/KeyboardMover.cs
@@ -10,11 +10,30 @@ public class KeyboardMover: MonoBehaviour {
     [Tooltip("Speed of movement, in meters per second")]
     [SerializeField] float speed = 1f;
 
+    float min_x_bound;
+    float max_x_bound;
+
+    void Start()
+    {
+        var camera_pos = GameObject.Find("Main Camera").GetComponent<Transform>().position;
+        var size = GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize * Screen.width / Screen.height;
+        min_x_bound = camera_pos.x - size;
+        max_x_bound = camera_pos.x + size;
+    }
+
     void Update() {
         float horizontal = Input.GetAxis("Horizontal"); // +1 if right arrow is pushed, -1 if left arrow is pushed, 0 otherwise
         float vertical = Input.GetAxis("Vertical");     // +1 if up arrow is pushed, -1 if down arrow is pushed, 0 otherwise
         Vector3 movementVector = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
         transform.position += movementVector;
+        if(transform.position.x >= max_x_bound)
+        {
+            transform.position = new Vector3(min_x_bound, transform.position.y, 0);
+        }
+        else if(transform.position.x <= min_x_bound)
+        {
+            transform.position = new Vector3(max_x_bound, transform.position.y, 0);
+        }
         //transform.Translate(movementVector);
         // NOTE: "Translate(movementVector)" uses relative coordinates - 
         //       it moves the object in the coordinate system of the object itself.
--- collisions/Assets/Scripts/1-movers/Mover.cs
+++ collisions/Assets/Scripts/1-movers/Mover.cs
@@ -9,11 +9,29 @@ public class Mover: MonoBehaviour {
     [Tooltip("Movement vector in meters per second")]
     [SerializeField] Vector3 velocity;
 
+    float min_y_bound;
+    float max_y_bound;
+
+    void Start()
+    {
+        var camera_pos = GameObject.Find("Main Camera").GetComponent<Transform>().position;
+        var size = GameObject.Find("Main Camera").GetComponent<Camera>().orthographicSize * Screen.width / Screen.height;
+        min_y_bound = camera_pos.y - size;
+        max_y_bound = camera_pos.y + size;
+    }
+
     void Update() {
         transform.position += velocity * Time.deltaTime;
+
+        var next_transform = transform.position;
+        if (next_transform.y > max_y_bound || next_transform.y < min_y_bound)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void SetVelocity(Vector3 newVelocity) {
         this.velocity = newVelocity;
     }
+
 }
